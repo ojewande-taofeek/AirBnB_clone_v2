bccb626031270d14795b6ae86cf44860544c9e07
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-06-02 12:08:16 +0100

    python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+    starts a Flask web application
+"""
+from flask import Flask, abort
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """Returns Hello"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    if text is None:
+        abort(404)
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
