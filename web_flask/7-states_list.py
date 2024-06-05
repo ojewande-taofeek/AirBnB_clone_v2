bbe388ea79fda08acdeef2bd8d1d59026a215e7a
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-06-05 18:52:40 +0100

    flask incorporated into AirBnB

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    starts a Flask web application
+    web application listens on 0.0.0.0, port 5000
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tearDown(exception):
+    """
+        remove the current SQLAlchemy Session
+        after each request
+    """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def listState():
+    """
+        List states in the storage alphabetically
+    """
+    all_states = storage.all(State)
+    return render_template("7-states_list.html",
+                           all_states=all_states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
